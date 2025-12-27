b013eff7a5afe3048cfbf1a4bbdcd5c79d20d7ad
app-kaisha <app.kaihatsusha@gmail.com>
2025-12-27 21:10:39 +0000

    Ch1.10 Basic animation and minium scale factor for UI

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -17,42 +17,32 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+                .animation(.default, value: imageName)
+            
             Spacer()
             
             Button("Show Message") {
-                //                let message1 = "You are Awesome!"
-                //                let message2 = "You are Great!"
-                //                let message3 = "Fabulous? That's You!"
-                //
-                //                if message == message1 {
-                //                    message = message2
-                //                } else if message == message2 {
-                //                    message = message3
-                //                } else {
-                //                    message = message1
-                //                }
                 
                 let messages = ["You Are Awesome!",
-                                "You Are Skilled!",
+                                "Gadzooks my friend! I am astonished at how utterly magnificent you are!",
                                 "You Are Great!",
                                 "Fabulous? That\'s You!",
                                 "You Are Fantastic!",
-                                "When the Genius Bar Need Help, They Call You!",
-                                "You Are A Code Monster!",
-                                "You Make Me Smile!",
-                                "I Think You Are Magic!"]
+                                "When the Genius Bar Need Help, They Call You!"]
                 
                 message = messages[messageNumber]
                 messageNumber += 1
@@ -60,24 +50,14 @@ struct ContentView: View {
                 if messageNumber == messages.count {
                     messageNumber = 0
                 }
-                //                message = messages[Int.random(in: 0..<messages.count)]
                 
-                
-                //                message = (message == message1 ? message2 : message1)
-                //                imageName = (imageName == "image0" ? "image1" : "image0")
-                //TODO: - update image name variable -
                 imageName="image\(imageNumber)"
-                
                 
                 imageNumber += 1
                 
                 if imageNumber > 9 {
                     imageNumber = 0
                 }
-                
-                print(imageNumber)
-                
-                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
